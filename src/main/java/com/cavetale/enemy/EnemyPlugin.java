e85a327b092a999faed05643b264e9f532a9f6c8
Thomas Loy <ths.loy@googlemail.com>
2021-01-01 15:07:47 +0100

    Hook up the Listener

--- src/main/java/com/cavetale/enemy/EnemyPlugin.java
+++ src/main/java/com/cavetale/enemy/EnemyPlugin.java
@@ -4,10 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnemyPlugin extends JavaPlugin {
     EnemyCommand enemyCommand = new EnemyCommand(this);
+    EnemyListener enemyListener = new EnemyListener(this);
 
     @Override
     public void onEnable() {
         enemyCommand.enable();
+        enemyListener.enable();
     }
 
     @Override
