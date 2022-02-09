19ce7bcf4024ffa0cdf08ea3d760b608684a8e17
StarTux <ths.loy@googlemail.com>
2022-02-09 13:07:51 +0100

    Fix Equipment and Vampirism (finally)

--- src/main/java/com/cavetale/enemy/ability/VampirismAbility.java
+++ src/main/java/com/cavetale/enemy/ability/VampirismAbility.java
@@ -2,7 +2,7 @@ package com.cavetale.enemy.ability;
 
 import com.cavetale.enemy.Context;
 import com.cavetale.enemy.Enemy;
-import com.cavetale.mytems.MytemsPlugin;
+import com.cavetale.mytems.gear.Equipment;
 import com.cavetale.mytems.item.acula.AculaItemSet;
 import lombok.Getter;
 import lombok.Setter;
@@ -53,32 +53,28 @@ public final class VampirismAbility extends AbstractAbility {
             if (!enemy.hasLineOfSight(player)) continue;
             Vector vec = player.getEyeLocation().subtract(eye).toVector().multiply(Math.random());
             Location particleLocation = eye.clone().add(vec);
-            double damageFactor = 1.0;
-            int ticksFactor = 1;
             if (isResistant(player)) {
                 enemy.getWorld().spawnParticle(Particle.REDSTONE, particleLocation, 1, 0, 0, 0, 0, new Particle.DustOptions(Color.BLUE, 1.0f));
-                damageFactor = 0.1;
-                ticksFactor = 9;
-            } else {
-                enemy.getWorld().spawnParticle(Particle.REDSTONE, particleLocation, 1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, 0.5f));
-                if (activeTicks % 16 == 0) {
-                    player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, SoundCategory.PLAYERS, 0.5f, 2.0f);
-                }
+                continue;
             }
-            double damage = Math.min(player.getHealth(), damagePerTick * damageFactor);
+            enemy.getWorld().spawnParticle(Particle.REDSTONE, particleLocation, 1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, 0.5f));
+            if (activeTicks % 16 == 0) {
+                player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, SoundCategory.PLAYERS, 0.5f, 2.0f);
+            }
+            double damage = Math.min(player.getHealth(), damagePerTick);
             player.setHealth(player.getHealth() - damage);
             enemy.setHealth(Math.min(enemy.getMaxHealth(), enemy.getHealth() + damage));
-            if (ticksPerHunger > 0 && activeTicks % (ticksPerHunger * ticksFactor) == 0) {
+            if (ticksPerHunger > 0 && (activeTicks % ticksPerHunger) == 0) {
                 player.setFoodLevel(Math.max(0, player.getFoodLevel() - 1));
             }
             if (saturationPerTick > 0f) {
-                player.setSaturation(Math.max(0, player.getSaturation() - saturationPerTick * (float) damageFactor));
+                player.setSaturation(Math.max(0, player.getSaturation() - saturationPerTick));
             }
         }
         return true;
     }
 
     public boolean isResistant(Player player) {
-        return MytemsPlugin.getInstance().getEquipment(player).hasSetBonus(AculaItemSet.getInstance().vampirismResistance);
+        return Equipment.of(player).hasSetBonus(AculaItemSet.getInstance().vampirismResistance);
     }
 }
