1f2d890487a2ca7b8605f75a8b79247dab32fe77
StarTux <ths.loy@googlemail.com>
2022-02-12 19:24:46 +0100

    Compromise

--- src/main/java/com/cavetale/enemy/boss/LivingBoss.java
+++ src/main/java/com/cavetale/enemy/boss/LivingBoss.java
@@ -41,7 +41,7 @@ public abstract class LivingBoss extends LivingEnemy implements TypedEnemy {
      */
     @Override
     public void onDefendingDamageCalculation(DamageCalculationEvent event) {
-        event.setIfApplicable(DamageFactor.ARMOR, value -> Math.min(0.4, value));
-        event.setIfApplicable(DamageFactor.PROTECTION, value -> Math.min(0.4, value));
+        event.setIfApplicable(DamageFactor.ARMOR, value -> Math.min(0.33, value));
+        event.setIfApplicable(DamageFactor.PROTECTION, value -> Math.min(0.33, value));
     }
 }
